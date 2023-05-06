e3a9912ea31f50f51a84a26fac907deb686c1588
hendrikbene <hendrikbene.perdok@gmail.com>
2023-05-06 15:22:23 +0200

    Update

--- Player.gd
+++ Player.gd
@@ -1,72 +1,105 @@
 extends CharacterBody2D
 
-@export var speed = 400
+# Movement variables
+@export var walk_speed : int  = 400
+@export var dash_speed : int  = 1200
+var is_dashing : bool = false
+var is_moving : bool = false
+var double_click_timer : Timer
+var double_click_count := 0
+var is_cooldown_active := false
+var dash_timer : Timer
+var dash_cooldown_timer : Timer
 
-var screen_size: Vector2 # Size of the game window.
-var target = position
-var animation_index = 0
+# Animation variables
+var current_state := "idle_down"
+var animated_sprite : AnimatedSprite2D
+
+# new vars
+var destination : Vector2 = Vector2(100, 100)
 
 func _ready():
-	screen_size = get_viewport_rect().size
+	animated_sprite = get_node("AnimatedSprite2D")
+	dash_cooldown_timer = get_node("DashCooldownTimer")
+	double_click_timer = get_node("DoubleClickTimer")
+	dash_timer = get_node("DashTimer")
+	dash_cooldown_timer.stop();
 
-func _unhandled_input(event):
+func _unhandled_input(event: InputEvent):
+	if event.is_action_pressed("left_mouse_button"):
+		destination = get_global_mouse_position()
+		_process_double_click_dash_input()
 
-	if event.is_action_pressed("l_m_b"):
-		target = get_global_mouse_position()
-		
 func _physics_process(_delta):
-	#http://kidscancode.org/godot_recipes/3.x/2d/8_direction/
-	velocity = position.direction_to(target).normalized() * speed
-	# look_at(target)
-	$AnimatedSprite2D.play()
-	animation_index = snapped(velocity.angle(), PI/4) / (PI/4)
-	animation_index = wrapi(int(animation_index), 0, 8)
-
-	if position.distance_to(target) > 10:
+	var movement
+	if position.distance_to(destination) > 10:
+		is_moving = true
+		current_state = _get_animation_state()
+		if is_dashing:
+			movement = position.direction_to(destination) * dash_speed
+		else:
+			movement = position.direction_to(destination) * walk_speed
+		velocity = movement
 		move_and_slide()
-		if animation_index == 2 || animation_index == 6:
-			if animation_index == 2:
-				$AnimatedSprite2D.animation = "walk_down"
-			elif animation_index == 6:
-				$AnimatedSprite2D.animation = "walk_up"
-		elif animation_index == 3 || animation_index == 4 || animation_index == 5:
-			$AnimatedSprite2D.animation = "walk"
-			$AnimatedSprite2D.flip_h = true
-		elif animation_index == 0 ||animation_index == 1 || animation_index == 7:
-			$AnimatedSprite2D.animation = "walk"
-			$AnimatedSprite2D.flip_h = false
+	elif position.distance_to(destination) < 10 and is_moving:
+		is_moving = false
+		current_state = _get_animation_state()
+		velocity = Vector2.ZERO
+
+	print("is_moving => ", is_moving)
+	print("State => ", current_state)
+	animated_sprite.play(current_state)
+
+func _process_double_click_dash_input():
+	if not is_cooldown_active:
+		if double_click_timer.is_stopped():
+			double_click_timer.start()
+		else:
+			double_click_count += 1
+
+		if double_click_count >= 2 and not is_dashing:
+			double_click_timer.stop()
+			dash_timer.start()
+			dash_cooldown_timer.start()
+			is_cooldown_active = true
+			is_dashing = true
+			double_click_count = 0
+
+func _get_animation_state() -> String:
+	var direction : String
+
+	if abs(velocity.x) > abs(velocity.y):
+		if velocity.x > 0:
+			direction = "right"
+			animated_sprite.flip_h = false
+		else:
+			direction = "left"
+			animated_sprite.flip_h = true
 	else:
-		$AnimatedSprite2D.animation = "idle"
-		$AnimatedSprite2D.stop()
+		if velocity.y > 0:
+			direction = "down"
+		else:
+			direction = "up"
+			animated_sprite.flip_h = true
+			
+	return _get_animation_state_prefix() + "_" + direction
+
+func _get_animation_state_prefix() -> String:
+	return "walk" if is_moving else "idle"
+
+func shoot():
+	pass
+
+func interact():
+	if Input.is_action_just_pressed("interact"):
+		pass
 
 
-	
-	# 8-DIRECTIONAL MOVEMENT/ANIMATION    current_animation = "idle"
+func _on_dash_cooldown_timer_timeout():
+	is_cooldown_active = false
 
+func _ondouble_click_timer_timeout():
+	double_click_count = 0
 
-
-	# if Input.is_action_pressed("l_m_b") and mouse.length() > 10:
-	# 	var current_animation: = "walk"
-	# 	move_and_slide()
-	# 	$AnimatedSprite2D.animation = current_animation + str(a)
-
-
-	# if velocity.x > 0 and velocity.y > 0:
-	# 	$AnimatedSprite2D.play("walk_down")
-	# elif velocity.x > 0 and velocity.y < 0:
-	# 	$AnimatedSprite2D.play("walk")
-	# 	$AnimatedSprite2D.flip_h = false
-	# elif velocity.x < 0 and velocity.y < 0:
-	# 	$AnimatedSprite2D.play("walk_up")
-	# elif velocity.x < 0 and velocity.y > 0:
-	# 	$AnimatedSprite2D.play("walk")
-	# 	$AnimatedSprite2D.flip_h = true
-	# animatie player using mouse direction dividing the screen in 4 sectors of
-
-	
-# func _process(_delta):
-# 	pass
-
-
-# # func _process(_delta): # every frame 
-# # 	focus_on_mouse()
+func _on_dash_timer_timeout():	
+	is_dashing = false
